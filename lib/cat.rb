d97aef6029947795e4a15f9ab06e67c80197fef4
Aida Abdikulova <abdikulova@gmail.com>
2019-07-28 21:49:22 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -4,9 +4,10 @@ class Cat
 
   @@all = []
 
-  def initialize(name_input,owner_input)
+  def initialize(name_input,owner_input,mood_input)
     @name = name_input
     @owner = owner_input
+    @mood = mood_input
   end
 
   def self.all
